b17fd7d06bbeb5980abd8f268452ac1fb833ac12
Ashish <ashishpahwa7@gmail.com>
2015-11-22 01:31:56 +0530

    added

---
+++ codezilla2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char a[100],b[50][50];
+        int j=0,i,t=0;
+while(t<5)
+{
+        gets(a);
+        for(i=0;i<5;i++)
+        {
+          b[i][j]=a[i];
+
+        }
+    j++;
+    t++;
+}
+
+        printf("%c
+
+
+}
+
